df2e94565e3e7d7f6714aad0fdfc4aad220c7bdf
Ahmadou Gueye <amdou69@gmail.com>
2021-04-29 13:15:57 +0000

    added fake employe generator

--- grh-api/src/Controller/EmployeController.php
+++ grh-api/src/Controller/EmployeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\CaisseSociale;
 use App\Entity\Employe;
+use App\Entity\Grade;
+use App\Entity\MutuelleSante;
+use App\Entity\Pays;
+use App\Entity\TypeEmploye;
 use App\Form\EmployeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,9 +33,9 @@ class EmployeController extends AbstractController
             ->getRepository(Employe::class)
             ->findAll();
 
-        return count($employes)?$employes:[];
+        return count($employes) ? $employes : [];
     }
-    
+
     /**
      * @Rest\Get(path="/{id}/typeemploye", name="employe_by_typeemploye")
      * @Rest\View(StatusCode = 200)
@@ -38,11 +43,12 @@ class EmployeController extends AbstractController
      */
     public function findByTypeEmployé(\App\Entity\TypeEmploye $typeEmploye): array
     {
+
         $employes = $this->getDoctrine()
             ->getRepository(Employe::class)
             ->findByTypeEmploye($typeEmploye);
 
-        return count($employes)?$employes:[];
+        return count($employes) ? $employes : [];
     }
 
     /**
@@ -50,20 +56,21 @@ class EmployeController extends AbstractController
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_CREATE")
      */
-    public function create(Request $request, \App\Service\FileUploader $uploader): Employe    {
+    public function create(Request $request, \App\Service\FileUploader $uploader): Employe
+    {
         $employe = new Employe();
         $form = $this->createForm(EmployeType::class, $employe);
         $form->submit(Utils::serializeRequestContent($request));
         $reqData = Utils::getObjectFromRequest($request);
-        if(!isset($reqData->dateNaissance)) {
+        if (!isset($reqData->dateNaissance)) {
             throw $this->createNotFoundException("La date de naissance est introuvable !");
         }
-        if(!isset($reqData->dateRecrutement)) {
+        if (!isset($reqData->dateRecrutement)) {
             throw $this->createNotFoundException("La date de recrutement est introuvable !");
         }
         $employe->setDateNaissance(new \DateTime($reqData->dateNaissance));
         $employe->setDateRecrutement(new \DateTime($reqData->dateRecrutement));
-        
+
         //check if file provided
         if ($employe->getFilepath()) {
             $host = $request->getHttpHost();
@@ -91,17 +98,19 @@ class EmployeController extends AbstractController
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_SHOW")
      */
-    public function show(Employe $employe): Employe    {
+    public function show(Employe $employe): Employe
+    {
         return $employe;
     }
 
-    
+
     /**
      * @Rest\Put(path="/{id}/edit", name="employe_edit",requirements = {"id"="\d+"})
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_EDIT")
      */
-    public function edit(Request $request, Employe $employe): Employe    {
+    public function edit(Request $request, Employe $employe): Employe
+    {
         $form = $this->createForm(EmployeType::class, $employe);
         $form->submit(Utils::serializeRequestContent($request));
 
@@ -109,15 +118,16 @@ class EmployeController extends AbstractController
 
         return $employe;
     }
-    
+
     /**
      * @Rest\Put(path="/{id}/clone", name="employe_clone",requirements = {"id"="\d+"})
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_CLONE")
      */
-    public function cloner(Request $request, Employe $employe):  Employe {
-        $em=$this->getDoctrine()->getManager();
-        $employeNew=new Employe();
+    public function cloner(Request $request, Employe $employe): Employe
+    {
+        $em = $this->getDoctrine()->getManager();
+        $employeNew = new Employe();
         $form = $this->createForm(EmployeType::class, $employeNew);
         $form->submit(Utils::serializeRequestContent($request));
         $em->persist($employeNew);
@@ -132,20 +142,22 @@ class EmployeController extends AbstractController
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_EDIT")
      */
-    public function delete(Employe $employe): Employe    {
+    public function delete(Employe $employe): Employe
+    {
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($employe);
         $entityManager->flush();
 
         return $employe;
     }
-    
+
     /**
      * @Rest\Post("/delete-selection/", name="employe_selection_delete")
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_EMPLOYE_DELETE")
      */
-    public function deleteMultiple(Request $request): array {
+    public function deleteMultiple(Request $request): array
+    {
         $entityManager = $this->getDoctrine()->getManager();
         $employes = Utils::getObjectFromRequest($request);
         if (!count($employes)) {
@@ -158,5 +170,54 @@ class EmployeController extends AbstractController
         $entityManager->flush();
 
         return $employes;
+    }
+
+    function generateFakeData()
+    {
+        // find groups
+        $em = $this->getDoctrine()->getManager();
+        $nationalites = $em->getRepository(Pays::class)->findAll();
+        $mutuelleSantes = $em->getRepository(MutuelleSante::class)->findAll();
+        $grades = $em->getRepository(Grade::class)->findAll();
+        $caisseSociales = $em->getRepository(CaisseSociale::class)->findAll();
+        $typeEmployes = $em->getRepository(TypeEmploye::class)->findAll();
+        $situationMatrimoniales = ['Célibataire', 'Marié(e)', 'Divorcé(e)', 'Veuf(ve)'];
+        $genres = ['Masculin', 'Féminin'];
+
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 1500; $i++) {
+            /** @var Employe $employe */
+            $employe = new Employe();
+            $employe
+                ->setPrenoms($faker->firstName)
+                ->setNom($faker->lastName)
+                ->setDateNaissance($faker->dateTime())
+                ->setLieuNaissance($faker->city)
+                ->setCni($faker->unique()->bankAccountNumber)
+                ->setMatricule($faker->unique()->postcode)
+                ->setMatriculeCaisseSociale($faker->unique()->postcode)
+                ->setDateRecrutement($faker->dateTime())
+                ->setSitutationMatrimoniale($faker->randomElement($situationMatrimoniales))
+                ->setRetraite($faker->boolean)
+                ->setGenre($faker->randomElement($genres))
+                ->setEtat($faker->boolean)
+                ->setEmailUniv(strtolower(str_replace(' ', '', $faker->unique()->name)) . "@univ-thies.sn")
+                ->setEmail($faker->unique()->email)
+                ->setFilename($faker->ean13)
+                ->setFilepath($faker->imageUrl())
+                ->setTelephonePrimaire($faker->unique()->phoneNumber)
+                ->setTelephoneSecondaire($faker->unique()->phoneNumber)
+                ->setCaisseSociale($faker->randomElement($caisseSociales))
+                ->setGrade($faker->randomElement($grades))
+                ->setMutuelleSante($faker->randomElement($mutuelleSantes))
+                ->setNationalite($faker->randomElement($nationalites))
+                ->setTypeEmploye($faker->randomElement($typeEmployes));
+
+
+            $em->persist($employe);
+        }
+        $em->flush();
     }
 }
