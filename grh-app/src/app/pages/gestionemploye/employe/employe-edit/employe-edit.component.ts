68119dfe1bf80c69159de75b714c572310dc8574
mbene dia <mbene.dia@univ-thies.sn>
2021-04-29 15:35:22 +0200

    mettre a jour employer-edit

--- grh-app/src/app/pages/gestionemploye/employe/employe-edit/employe-edit.component.ts
+++ grh-app/src/app/pages/gestionemploye/employe/employe-edit/employe-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ViewChildren, EventEmitter, Output, Input, ElementRef } from '@angular/core';
 import { Employe } from '../employe';
 import { EmployeService } from '../employe.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -7,6 +7,16 @@ import { Store } from '@ngrx/store';
 import { IAppState } from 'src/app/interfaces/app-state';
 import { BasePageComponent } from 'src/app/pages/base-page';
 import { Location } from '@angular/common';
+import { UploadFileModel } from 'src/app/shared/classes/upload-file-model';
+import { TypeEmploye } from 'src/app/pages/parametrage/typeemploye/typeemploye';
+import { Pays } from 'src/app/pages/parametrage/pays/pays';
+import { CaisseSociale } from 'src/app/pages/parametrage/caissesociale/caissesociale';
+import { MutuelleSante } from 'src/app/pages/parametrage/mutuellesante/mutuellesante';
+import { Grade } from 'src/app/pages/gestiongrade/grade/grade';
+import { PaysService } from 'src/app/pages/parametrage/pays/pays.service';
+import { MutuelleSanteService } from 'src/app/pages/parametrage/mutuellesante/mutuellesante.service';
+import { CaisseSocialeService } from 'src/app/pages/parametrage/caissesociale/caissesociale.service';
+import { GradeService } from 'src/app/pages/gestiongrade/grade/grade.service';
 
 @Component({
   selector: 'app-employe-edit',
@@ -15,10 +25,32 @@ import { Location } from '@angular/common';
 })
 export class EmployeEditComponent extends BasePageComponent<Employe> implements OnInit, OnDestroy {
 
+  @ViewChild('modalBody', { static: true }) modalBody: ElementRef<any>;
+  @ViewChild('modalFooter', { static: true }) modalFooter: ElementRef<any>;
+  @ViewChildren('form') form;
+  entity: Employe;
+  @Output() creation: EventEmitter<Employe> = new EventEmitter();
+  isModalVisible = false;
+  currentAvatar: any;
+  fileModel: UploadFileModel = new UploadFileModel();
+  @Input() typeEmploye: TypeEmploye;
+  nationalites: Pays[] = [];
+  selectedNationaliteId: any;
+  caisseSociales: CaisseSociale[] = [];
+  selectedCaisseSocialeId: any;
+  mutuelleSantes: MutuelleSante[] = [];
+  selectedMutuelleSanteId: any;
+  grades: Grade[] = [];
+  selectedGradeId: any;
+
   constructor(store: Store<IAppState>,
               public employeSrv: EmployeService,
               public router: Router,
+              public paysSrv: PaysService,
+              public mutuelleSanteSrv: MutuelleSanteService,
+              public caisseSocialeSrv: CaisseSocialeService,
               private activatedRoute: ActivatedRoute,
+              public gradeSrv: GradeService,
               public location: Location) {
     super(store, employeSrv);
     this.pageData = {
@@ -44,18 +76,79 @@ export class EmployeEditComponent extends BasePageComponent<Employe> implements 
     this.findEntity(this.activatedRoute.snapshot.params.id);
   }
 
+  // open modal window
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  // close modal window
+  closeModal() {
+    this.isModalVisible = false;
+  }
+
+  onFileChanged(inputValue: any) {
+    let file: File = inputValue.target.files[0];
+    let reader: FileReader = new FileReader();
+    reader.onloadend = () => {
+      this.currentAvatar = reader.result;
+      this.fileModel.fileName = file.name.split('.')[0];
+      this.fileModel.fileContent = this.currentAvatar.split(',')[1];
+    };
+    reader.readAsDataURL(file);
+  }
+  findNationalites() {
+    this.paysSrv.findAll()
+      .subscribe((data: any) => {
+        this.nationalites = data;
+      }, err => this.paysSrv.httpSrv.catchError(err));
+  }
+
+  findMutuelleSantes() {
+    this.mutuelleSanteSrv.findAll()
+      .subscribe((data: any) => {
+        this.mutuelleSantes = data;
+      }, err => this.mutuelleSanteSrv.httpSrv.catchError(err));
+  }
+
+  findCaisseSociales() {
+    this.caisseSocialeSrv.findAll()
+      .subscribe((data: any) => {
+        this.caisseSociales = data;
+      }, err => this.caisseSocialeSrv.httpSrv.catchError(err));
+  }
+
+  findGrades() {
+    this.gradeSrv.findAll()
+      .subscribe((data: any) => {
+        this.grades = data;
+      }, err => this.gradeSrv.httpSrv.catchError(err));
+  }
+
   ngOnDestroy() {
     super.ngOnDestroy();
   }
 
   handlePostLoad() {
+    this.selectedMutuelleSanteId = this.entity.mutuelleSante?.id;
+    this.selectedCaisseSocialeId = this.entity.caisseSociale?.id;
+    this.selectedGradeId = this.entity.grade?.id;
+    this.selectedNationaliteId = this.entity.nationalite?.id;
+    this.findCaisseSociales();
+    this.findMutuelleSantes();
+    this.findNationalites();
+    this.findGrades();
   }
 
   prepareUpdate() {
+    this.entity.caisseSociale = this.selectedCaisseSocialeId;
+    this.entity.grade = this.selectedGradeId;
+    this.entity.nationalite = this.selectedNationaliteId;
+    this.entity.mutuelleSante = this.selectedMutuelleSanteId;
   }
 
   handlePostUpdate() {
     this.location.back();
   }
+
 
 }
