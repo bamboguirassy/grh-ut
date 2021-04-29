30d6b476491cde613496ff0b7b5f5c65ca6f7f69
bamboguirassy <didegassama@gmail.com>
2021-04-29 13:13:53 +0000

    implementation des statistiques de count d'employes - dashboard

--- grh-app/src/app/shared/services/bambo-abstract.service.ts
+++ grh-app/src/app/shared/services/bambo-abstract.service.ts
@@ -11,27 +11,27 @@ export abstract class BamboAbstractService {
   constructor(public httpSrv: BamboHttpService, public toastr: ToastrService) { }
 
   findAll() {
-    return this.httpSrv.get(this.routePrefix);
+    return this.httpSrv.get(this.routePrefix).pipe(first());
   }
 
   findOneById(id: number) {
-    return this.httpSrv.get(this.routePrefix + id);
+    return this.httpSrv.get(this.routePrefix + id).pipe(first());
   }
 
   create(item: BamboAbstractObject) {
-    return this.httpSrv.post(this.routePrefix + 'create', item);
+    return this.httpSrv.post(this.routePrefix + 'create', item).pipe(first());
   }
 
   update(item: BamboAbstractObject) {
-    return this.httpSrv.put(this.routePrefix + item.id + '/edit', item);
+    return this.httpSrv.put(this.routePrefix + item.id + '/edit', item).pipe(first());
   }
 
   clone(originalId: number, clone: BamboAbstractObject) {
-    return this.httpSrv.put(this.routePrefix + originalId + '/clone', clone);
+    return this.httpSrv.put(this.routePrefix + originalId + '/clone', clone).pipe(first());
   }
 
   remove(item: BamboAbstractObject) {
-    return this.httpSrv.delete(this.routePrefix + item.id);
+    return this.httpSrv.delete(this.routePrefix + item.id).pipe(first());
   }
 
   getData(url) {
