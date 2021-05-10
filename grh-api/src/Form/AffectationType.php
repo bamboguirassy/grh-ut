de36ff7fdfe032d505bfb0ab1e2f90a4c90cf790
bamboguirassy <didegassama@gmail.com>
2021-05-10 14:44:16 +0000

    https://uidt.atlassian.net/browse/GRH-41?atlOrigin=eyJpIjoiZTRjYzY5Y2M2MDEyNGVhNWI3ZGI2YTFjYTEzNDQxNjIiLCJwIjoiaiJ9

---
+++ grh-api/src/Form/AffectationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Affectation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AffectationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('motif')
+            ->add('poste')
+            ->add('employe')
+            ->add('structure')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Affectation::class,
+        ]);
+    }
+}
