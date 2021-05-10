977a25e174f761896a80aca8b3577f47c0d9fc2b
bamboguirassy <didegassama@gmail.com>
2021-05-10 12:46:02 +0000

    catching error in delete and update methods - base components

--- grh-app/src/app/pages/base-page/base-page.component.ts
+++ grh-app/src/app/pages/base-page/base-page.component.ts
@@ -104,7 +104,7 @@ export class BasePageComponent<T extends BamboAbstractObject> implements OnInit,
     this.httpSv.update(this.entity)
       .subscribe((resp) => {
         this.handlePostUpdate();
-      });
+      },err=>this.httpSv.httpSrv.catchError(err));
   }
 
   prepareUpdate() {
@@ -131,7 +131,7 @@ export class BasePageComponent<T extends BamboAbstractObject> implements OnInit,
             Swal.close();
             this.httpSv.toastr.success("Suppression reussie");
             this.handlePostDelete();
-          });
+          },err=>this.httpSv.httpSrv.catchError(err));
         // For more information about handling dismissals please visit
         // https://sweetalert2.github.io/#handling-dismissals
       } else if (result.dismiss === Swal.DismissReason.cancel) {
