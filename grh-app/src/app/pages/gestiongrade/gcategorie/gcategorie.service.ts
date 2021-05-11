f2c26d2aefc893adcae09e202666d0592faad01a
bamboguirassy <didegassama@gmail.com>
2021-05-11 13:25:26 +0000

    https://uidt.atlassian.net/browse/GRH-13?atlOrigin=eyJpIjoiMGVjZjQ0YWM1MThjNGQ1MDlhMWE2ZjgxYWNkYmI3ZjQiLCJwIjoiaiJ9

--- grh-app/src/app/pages/gestiongrade/gcategorie/gcategorie.service.ts
+++ grh-app/src/app/pages/gestiongrade/gcategorie/gcategorie.service.ts
@@ -2,6 +2,7 @@ import { BamboAbstractService } from '../../../shared/services/bambo-abstract.se
 import { BamboHttpService } from './../../../shared/services/bambo-http.service';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class GCategorieService extends BamboAbstractService {
     super(httpSrv, toastr);
     this.routePrefix = 'gcategorie/';
     this.resourceName = 'GCATEGORIE';
+  }
+
+  findNonSuivants() {
+    return this.httpSrv.get(this.routePrefix+'no-suivant/').pipe(first());
   }
   
 }
