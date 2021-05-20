66410997ff7afd64836a19cae8f2e7a7aa75e468
bamboguirassy <didegassama@gmail.com>
2021-05-20 20:54:39 +0000

    fixing error typage dans recrutement-period-stats de codé ndiaye

--- grh-app/src/app/pages/dashboards/recrutement-period/recrutement-period-stats.ts
+++ grh-app/src/app/pages/dashboards/recrutement-period/recrutement-period-stats.ts
@@ -1,2 +1,8 @@
+import { TypeEmploye } from "../../parametrage/typeemploye/typeemploye"
+
 export class RecrutementPeriodStats {
+    nbreEmploye: number;
+    nbrHomme: number;
+    nbrFemme: number;
+    typeEmploye: TypeEmploye;
 }
