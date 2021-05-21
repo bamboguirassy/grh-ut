a4515688c70694696e67a60f0db2edaf5628613f
Aminata Samb Fofana <aminata.samb@univ-thies.sn>
2021-05-21 16:38:07 +0000

    https://uidt.atlassian.net/browse/GRH-95

--- grh-app/src/app/pages/parametrage/syndicat/syndicat.columns.ts
+++ grh-app/src/app/pages/parametrage/syndicat/syndicat.columns.ts
@@ -1,6 +1,7 @@
 const syndicatColumns = [
             { header: 'Nom', field: 'nom', dataKey: 'nom' },
             { header: 'Code', field: 'code', dataKey: 'code' },
+            { header: 'Type', field: 'type', dataKey: 'type' },
             { header: 'Filename', field: 'filename', dataKey: 'filename' },
             { header: 'Filepath', field: 'filepath', dataKey: 'filepath' },
             { header: 'MontantCotisationMensuelle', field: 'montantCotisationMensuelle', dataKey: 'montantCotisationMensuelle' },
@@ -9,6 +10,7 @@ const syndicatColumns = [
 const allowedSyndicatFieldsForFilter = [
     'nom',
     'code',
+    'type',
     'filename',
     'filepath',
     'montantCotisationMensuelle',
