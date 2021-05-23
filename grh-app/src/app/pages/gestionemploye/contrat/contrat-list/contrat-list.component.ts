b9836d389b1445077451cbb4c9c897231d46da1d
bamboguirassy <didegassama@gmail.com>
2021-05-23 14:58:32 +0000

    https://uidt.atlassian.net/browse/GRH-89

--- grh-app/src/app/pages/gestionemploye/contrat/contrat-list/contrat-list.component.ts
+++ grh-app/src/app/pages/gestionemploye/contrat/contrat-list/contrat-list.component.ts
@@ -26,8 +26,6 @@ export class ContratListComponent implements OnInit, OnDestroy {
 
   }
 
-  
-
   ngOnInit(): void {
     this.findByEmploye();
   }
@@ -67,7 +65,7 @@ export class ContratListComponent implements OnInit, OnDestroy {
   }
   
   onCreate(item: Contrat) {
-    this.items = [item, ...this.items];
+    this.findByEmploye();
   }
 
   onClose(){
