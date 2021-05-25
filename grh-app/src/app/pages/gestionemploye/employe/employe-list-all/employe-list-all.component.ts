8ff104085125d0a59be0d896052bd90fd5787be6
El Hadji Code Ndiaye <elhadjicode.ndiaye@univ-thies.sn>
2021-05-25 09:37:59 +0000

    https://uidt.atlassian.net/browse/GRH-82

---
+++ grh-app/src/app/pages/gestionemploye/employe/employe-list-all/employe-list-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employe-list-all',
+  templateUrl: './employe-list-all.component.html',
+  styleUrls: ['./employe-list-all.component.scss']
+})
+export class EmployeListAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
