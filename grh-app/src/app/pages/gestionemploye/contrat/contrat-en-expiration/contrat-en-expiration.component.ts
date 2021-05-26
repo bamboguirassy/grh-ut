3717919a34c88141a7ecb57a2aaebf9fc1639e3e
bamboguirassy <didegassama@gmail.com>
2021-05-26 11:12:45 +0000

    https://uidt.atlassian.net/browse/GRH-97

--- grh-app/src/app/pages/gestionemploye/contrat/contrat-en-expiration/contrat-en-expiration.component.ts
+++ grh-app/src/app/pages/gestionemploye/contrat/contrat-en-expiration/contrat-en-expiration.component.ts
@@ -6,6 +6,7 @@ import Swal from 'sweetalert2';
 import { ContratService } from '../contrat.service';
 import { Contrat } from '../contrat';
 import { BasePageComponent } from 'src/app/pages/base-page';
+import { EmployeService } from '../../employe/employe.service';
 
 @Component({
   selector: 'app-contrat-en-expiration',
@@ -21,7 +22,8 @@ export class ContratEnExpirationComponent extends BasePageComponent<Contrat>  im
 
 
   constructor(store: Store<IAppState>,
-    public contratSrv: ContratService) {
+    public contratSrv: ContratService,
+    public employeSrv: EmployeService) {
       super(store, contratSrv);
       this.setLoaded(); 
       this.pageData = {
