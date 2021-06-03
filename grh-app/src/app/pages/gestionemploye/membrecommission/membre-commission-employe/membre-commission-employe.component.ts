8fdcf124fd4e10fed08d78a8e93bf303e2157ff4
yuri2397 <diawlemor97@gmail.com>
2021-06-03 00:03:19 +0000

    https://uidt.atlassian.net/browse/GRH-136

--- grh-app/src/app/pages/gestionemploye/membrecommission/membre-commission-employe/membre-commission-employe.component.ts
+++ grh-app/src/app/pages/gestionemploye/membrecommission/membre-commission-employe/membre-commission-employe.component.ts
@@ -1,20 +1,22 @@
 import {Component, Input, OnDestroy, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 import {Store} from "@ngrx/store";
 import {IAppState} from "src/app/interfaces/app-state";
 import {BasePageComponent} from "src/app/pages/base-page/base-page.component";
 import {MembreCommission} from "src/app/pages/gestionemploye/membrecommission/membrecommission";
 import {MembreCommissionService} from "src/app/pages/gestionemploye/membrecommission/membrecommission.service";
-import { Commission } from "src/app/pages/parametrage/commission/commission";
+import {Commission} from "src/app/pages/parametrage/commission/commission";
+import { Employe } from "../../employe/employe";
 
 @Component({selector: "app-membre-commission-employe", templateUrl: "./membre-commission-employe.component.html", styleUrls: ["./membre-commission-employe.component.scss"]})
 export class MembreCommissionEmployeComponent
 extends BasePageComponent<MembreCommission>
 implements OnInit,
 OnDestroy {
-  @Input() commission: Commission;
+  @Input()commission: Commission;
   isShowable: boolean = false;
 
-  constructor(store : Store<IAppState>,private membreCommissionSrv : MembreCommissionService) {
+  constructor(store : Store<IAppState>, private membreCommissionSrv : MembreCommissionService, private router : Router) {
     super(store, membreCommissionSrv);
   }
 
@@ -23,12 +25,20 @@ OnDestroy {
     this.findMembresOfCommission(this.commission);
   }
 
-  private findMembresOfCommission(commission: Commission){
-    this.membreCommissionSrv.findMembresOfCommission(commission)
-    .subscribe((data: any)=>{
-      this.isShowable = true;
-      this.items = data;
-    },err=>this.membreCommissionSrv.httpSrv.catchError(err));
+  /**
+   * Permet d'avoir le route pour voir les infos de l'employé
+   * @param employe employe
+   * @returns route
+   */
+  employeRoute(employe : Employe): string[]{
+    let url: string = this.router.url;
+    return ["/" + url.split("/")[1] + "/employe/" + employe.id];
   }
 
+  private findMembresOfCommission(commission : Commission) {
+    this.membreCommissionSrv.findMembresOfCommission(commission).subscribe((data : any) => {
+      this.isShowable = true;
+      this.items = data;
+    }, (err) => this.membreCommissionSrv.httpSrv.catchError(err));
+  }
 }
