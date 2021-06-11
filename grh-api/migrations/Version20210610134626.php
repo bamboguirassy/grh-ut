1df13ebcceb069b2667e749ea08bb25849eba3a2
bamboguirassy <didegassama@gmail.com>
2021-06-11 09:26:06 +0000

    fixing migrating

--- grh-api/migrations/Version20210610134626.php
+++ grh-api/migrations/Version20210610134626.php
@@ -22,12 +22,11 @@ final class Version20210610134626 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP INDEX cni ON employe');
         $this->addSql('ALTER TABLE rang ADD appelation VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE responsabilite ADD rang_id INT NOT NULL');
         $this->addSql('ALTER TABLE responsabilite ADD CONSTRAINT FK_4EA098203CC0D837 FOREIGN KEY (rang_id) REFERENCES rang (id)');
         $this->addSql('CREATE INDEX IDX_4EA098203CC0D837 ON responsabilite (rang_id)');
-        $this->addSql('ALTER TABLE structure_fonction ADD rang INT NOT NULL');
+        $this->addSql('ALTER TABLE structure_fonction ADD rang INT');
         $this->addSql('ALTER TABLE structure_fonction ADD CONSTRAINT FK_580577F781CFA4CE FOREIGN KEY (rang) REFERENCES rang (id)');
         $this->addSql('CREATE INDEX IDX_580577F781CFA4CE ON structure_fonction (rang)');
     }
