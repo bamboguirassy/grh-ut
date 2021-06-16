c25edcb9d9c3e595c7771cb06ba1e9fb1bfeeb27
El Hadji Code Ndiaye <elhadjicode.ndiaye@univ-thies.sn>
2021-06-16 09:43:42 +0000

    Créer  les attributs interimaire et appelation respectivement dans fonctionemploye et structurefonction

--- grh-api/migrations/Version20210616093929.php
+++ grh-api/migrations/Version20210616093929.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210616090741 extends AbstractMigration
+final class Version20210616093929 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,10 +24,10 @@ final class Version20210616090741 extends AbstractMigration
 
         $this->addSql('DROP INDEX cni ON employe');
         $this->addSql('ALTER TABLE employe CHANGE cni cni VARCHAR(45) NOT NULL');
-        $this->addSql('ALTER TABLE fonction_employe CHANGE interimaire interimaire TINYINT(1) DEFAULT NULL COMMENT \'true ou false pour savoir si la fonction est intérimaire ou pas\'');
+        $this->addSql('ALTER TABLE fonction_employe ADD interimaire TINYINT(1) DEFAULT NULL COMMENT \'true ou false pour savoir si la fonction est intérimaire ou pas\'');
         $this->addSql('ALTER TABLE membre_syndicat CHANGE etat etat TINYINT(1) DEFAULT NULL COMMENT \'true si c\'\'est le syndicat actuel du travailleur
         un employé peut être dans plusieurs syndicats\'');
-        $this->addSql('ALTER TABLE structure_fonction CHANGE appelation appelation VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE structure_fonction ADD appelation VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE structure_fonction ADD CONSTRAINT FK_580577F781CFA4CE FOREIGN KEY (rang) REFERENCES rang (id)');
     }
 
@@ -38,9 +38,9 @@ final class Version20210616090741 extends AbstractMigration
 
         $this->addSql('ALTER TABLE employe CHANGE cni cni VARCHAR(45) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('CREATE UNIQUE INDEX cni ON employe (cni)');
-        $this->addSql('ALTER TABLE fonction_employe CHANGE interimaire interimaire TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE fonction_employe DROP interimaire');
         $this->addSql('ALTER TABLE membre_syndicat CHANGE etat etat TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE structure_fonction DROP FOREIGN KEY FK_580577F781CFA4CE');
-        $this->addSql('ALTER TABLE structure_fonction CHANGE appelation appelation VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE structure_fonction DROP appelation');
     }
 }
--- grh-app/src/app/pages/gestionemploye/fonctionemploye/fonctionemploye-timeline/fonctionemploye-timeline.component.ts
+++ grh-app/src/app/pages/gestionemploye/fonctionemploye/fonctionemploye-timeline/fonctionemploye-timeline.component.ts
@@ -34,7 +34,7 @@ export class FonctionemployeTimelineComponent implements OnInit {
         :'Indefini'} - ${i.etat?'En Cours'
         :`${i.dateFin?`${this.datePipe.transform(`${i.dateFin}`
          , 'dd/MM/yyyy')}`:'En Cours'}`}`,
-        title: i.responsabilite.fonction.nom,
+        title: i.responsabilite?.rang?.nom,
         content: i.responsabilite.structure.nom,
         icon: "icofont-business-man-alt-1",
         iconBg: SETTINGS.sidebarBg,
