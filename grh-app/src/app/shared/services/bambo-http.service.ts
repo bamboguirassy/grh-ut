3590823f854734f008ca39133a361c56de0d3823
bamboguirassy <didegassama@gmail.com>
2021-06-24 10:11:26 +0000

    restoring local link in develop branch

--- grh-app/src/app/shared/services/bambo-http.service.ts
+++ grh-app/src/app/shared/services/bambo-http.service.ts
@@ -11,9 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class BamboHttpService {
 
-  // public baseUrl = 'http://127.0.1:8000/api/';
-  public baseUrl = 'https://grh-ws.uidt.sn/index.php/api/';
-  
+  public baseUrl = 'http://127.0.1:8000/api/';
   private httpOptions: any = null;
 
   constructor(public http: HttpClient, public tokenSrv: BamboTokenService,
